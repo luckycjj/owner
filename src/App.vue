f9d189490427068b58b01ae386d9dcc06dca8dec
luckycjj <512170894@qq.com>
2018-10-22 11:18:03 +0800

    个人信息

--- src/App.vue
+++ src/App.vue
@@ -45,6 +45,38 @@
       if(cookie != "" && sessionStorage.getItem("addPageList")*1 == 0){
         cookie = JSON.parse(cookie);
         sessionStorage.setItem("token",cookie.token);
+        $.ajax({
+          type: "POST",
+          url: androidIos.ajaxHttp() + "/getUserInfo",
+          data:JSON.stringify({
+            userCode:sessionStorage.getItem("token"),
+            source:sessionStorage.getItem("source")
+          }),
+          contentType: "application/json;charset=utf-8",
+          dataType: "json",
+          timeout: 30000,
+          success: function (getUserInfo) {
+            if (getUserInfo.success == "1") {
+              sessionStorage.setItem("ownerMessage",JSON.stringify({
+                licType: getUserInfo.licType,
+                name:  getUserInfo.name,
+                photo:  getUserInfo.photo,
+                status:  getUserInfo.status,
+                corpName:  getUserInfo.corpName,
+                role:getUserInfo.role
+              }));
+            }else{
+              androidIos.second(getUserInfo.message);
+            }
+          },
+          complete: function (XMLHttpRequest, status) { //请求完成后最终执行参数
+            if (status == 'timeout') { //超时,status还有success,error等值的情况
+              androidIos.second("当前状况下网络状态差，请检查网络！");
+            } else if (status == "error") {
+              androidIos.errorwife();
+            }
+          }
+        });
         _this.$router.push({ path: '/xinYaIndex'});
       }else if(cookie == ""){
         _this.$router.push({ path: '/xinYaIndex'});
