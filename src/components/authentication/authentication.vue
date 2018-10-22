fa0f83f29caf6deb1e2d0d4333dce62ccf9ee021
luckycjj <512170894@qq.com>
2018-10-22 10:14:04 +0800

    倒计时

--- src/components/authentication/authentication.vue
+++ src/components/authentication/authentication.vue
@@ -282,6 +282,9 @@
                timeout: 30000,
                success: function (json) {
                  if(json.result.score > 0.95){
+                   clearInterval(_this.setTime);
+                   _this.baidudumaHaoma = "";
+                   _this.daojishi = 0;
                    $.ajax({
                      type: "POST",
                      url: androidIos.ajaxHttp() + "/uploadFile",
@@ -373,7 +376,8 @@
            var _this = this;
            _this.baidudumaBox  = false;
            clearInterval(_this.setTime);
-           _this.daojishi = 60;
+           _this.baidudumaHaoma = "";
+           _this.daojishi = 0;
          },
           go:function(){
              var _this = this;
