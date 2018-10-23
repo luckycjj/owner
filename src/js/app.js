51b3302f0cf26f735885b9dbd7d6c5a3cf0e517b
luckycjj <512170894@qq.com>
2018-10-23 13:56:22 +0800

    自适应iphoneX

--- src/js/app.js
+++ src/js/app.js
@@ -694,8 +694,8 @@ var androidIos = {
       paddingTop = api.safeArea.top + "px";
     }
     catch (e){
-      paddingBottom = 20 + "px";
-      paddingTop = 20 + "px";
+      paddingBottom = 0 + "px";
+      paddingTop = 0 + "px";
     }
     var docuId = document.getElementById(id);
     if(docuId == null){
